d9c971138a24664cb3bef898fa4d09803cace980
Jaedson Silva <contacanal798@gmail.com>
2021-10-28 17:12:08 -0300

    criando ping simples

---
+++ Ping/ping_simples.py
@@ -0,0 +1,13 @@
+import os
+
+def test_host(host, request_number) -> None:
+	print('\nIniciando...\n')
+	os.system(f'ping -c {request_number} {host}')
+
+print('\n\033[47;30m Ping Test \033[m\n')
+
+host = str(input('Digite o HOST: ')).strip()
+req_number = int(input('Quantidade de requisições (Ex: 6): '))
+
+test_host(host, req_number)
+
